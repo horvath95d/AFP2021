ea20b130ce7e8c9bb1d0aa725cd6c2a8d9650f47
Szalóki Dávid <Szunyix@gmail.com>
2021-02-17 18:03:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>WEB2</title>
+	<!-- boostrep  -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<!-- saját css	-->
+	<link rel="stylesheet" type="text/css" href="<?=PUBLIC_DIR.'style.css'?>">
+</head>
+
+<body> <!-- cont f5 -->
+
+
+</body>
+
+</html>
